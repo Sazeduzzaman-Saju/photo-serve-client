3e67b55fc0510e25c87551cd6baebc44ed6d7883
Sazeduzzaman Saju <sazeduzzaman@yahoo.com>
2022-11-07 22:17:30 +0600

    Stracture ready

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className='container mx-auto px-4'>
+            <h1 className='flex justify-start'>This is Home</h1>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/Routes/Routes.js
@@ -0,0 +1,17 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../layout/Main";
+import Home from "../Pages/Home/Home";
+import About from "../Pages/About/About";
+import Login from "../Pages/Login/Login";
+
+const router = createBrowserRouter([
+    {
+        path: '/', element: <Main />, children: [
+            { path: '/home', element: <Home /> },
+            { path: '/about', element: <About /> },
+            { path: '/login', element: <Login /> }
+        ]
+    }
+]);
+
+export default router;
